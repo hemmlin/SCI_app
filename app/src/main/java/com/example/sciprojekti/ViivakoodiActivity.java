796d54f9f28a3602e6ffbd8b834be00b38cd0d59
Linda Hemmann <linda.hemmann@gmail.com>
2020-02-25 19:22:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sciprojekti/ViivakoodiActivity.java
@@ -0,0 +1,14 @@
+package com.example.sciprojekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViivakoodiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viivakoodi);
+    }
+}
