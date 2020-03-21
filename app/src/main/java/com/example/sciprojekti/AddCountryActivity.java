33354603e64838368bf249774979ef4dc87deff6
Linda Hemmann <linda.hemmann@gmail.com>
2020-03-21 17:24:03 +0200

    Searchable spinneri ja listan ulkoasua muokattu

--- app/src/main/java/com/example/sciprojekti/AddCountryActivity.java
+++ app/src/main/java/com/example/sciprojekti/AddCountryActivity.java
@@ -12,6 +12,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddCountryActivity extends Activity implements OnClickListener {
 
     private Button addTodoBtn;
@@ -38,33 +43,58 @@ public class AddCountryActivity extends Activity implements OnClickListener {
         //subjectEditText = (EditText) findViewById(R.id.subject_edittext);
         descEditText = (EditText) findViewById(R.id.description_edittext);
 
+        List<String> searchablespinner_list = new ArrayList<String>();
+        SearchableSpinner searchablespinner = (SearchableSpinner) findViewById(R.id.searchablespinner);
 
+
+        searchablespinner_list.add("Maito Valio");
+        searchablespinner_list.add("Omenamehu Pirkka");
+        searchablespinner_list.add("Ruisleipä Vaasan");
+        searchablespinner_list.add("Banaani");
+        searchablespinner_list.add("Omena");
+        searchablespinner_list.add("Suklaalevy Fazer");
+        searchablespinner_list.add("Nyhtökaura");
+        searchablespinner_list.add("Härkis");
+        searchablespinner_list.add("Jauhelliha");
+        searchablespinner_list.add("Riisi");
+        searchablespinner_list.add("Vessapaperi");
+
+        searchablespinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, searchablespinner_list));
         addTodoBtn = (Button) findViewById(R.id.add_record);
 
         dbManager = new DBManager(this);
         dbManager.open();
         addTodoBtn.setOnClickListener(this);
-        addListenerOnSpinnerItemSelection();
+       // addListenerOnSpinnerItemSelection();
+
 
 
     }
-    public void addListenerOnSpinnerItemSelection() {
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
+   // public void addListenerOnSpinnerItemSelection() {
+      //  spinner1 = (Spinner) findViewById(R.id.spinner1);
+        //spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        //spinner2 = (Spinner) findViewById(R.id.searchablespinner);
+
+
+   // }
     // get the selected dropdown list value
-    public void addListenerOnButton() {
+   // public void addListenerOnButton() {
 
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
+       // spinner1 = (Spinner) findViewById(R.id.spinner1);
+        //spinner2= (Spinner) findViewById(R.id.searchablespinner);
         //spinner2 = (Spinner) findViewById(R.id.spinner2);
-    }
+   // }
 
     //spinnerihommat loppuu
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add_record:
-                final String tuote= String.valueOf(spinner1.getSelectedItem());
+                SearchableSpinner spinner;
+                spinner = (SearchableSpinner) findViewById(R.id.searchablespinner);
+                spinner.setTitle("Tuote");
+                spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+                final String tuote= String.valueOf(spinner.getSelectedItem());
                 //final String name = subjectEditText.getText().toString();
                 final String desc = descEditText.getText().toString();
 
