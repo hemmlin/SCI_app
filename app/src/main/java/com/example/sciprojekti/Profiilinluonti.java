0e04986ae0f8d3c0a0f4902e9ca93825cf4a5fc7
emma <emma.helina.jarvinen@gmail.com>
2020-03-25 17:19:08 +0200

    Profiilikuvan lisääminen (tallennus ei toimi)

--- app/src/main/java/com/example/sciprojekti/Profiilinluonti.java
+++ app/src/main/java/com/example/sciprojekti/Profiilinluonti.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
